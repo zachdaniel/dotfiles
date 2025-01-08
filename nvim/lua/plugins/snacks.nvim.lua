1662f8c709e4a192cd0c9ed58e3ea237d2e195c1
Zach Daniel <zach@zachdaniel.dev>
2025-01-07 23:14:58 -0500

    WIP

--- nvim/lua/plugins/snacks.nvim.lua
+++ nvim/lua/plugins/snacks.nvim.lua
@@ -13,7 +13,7 @@ return {
         { section = "header" },
         { section = "keys",  gap = 1, padding = 1 },
         {
-          pane = 2,
+          pane = 1,
           icon = " ",
           desc = "Browse Repo",
           padding = 1,
@@ -25,43 +25,6 @@ return {
         function()
           local in_git = Snacks.git.get_root() ~= nil
           local cmds = {
-            {
-              title = "Notifications",
-              cmd = "gh notify -s -a -n5",
-              action = function()
-                vim.ui.open("https://github.com/notifications")
-              end,
-              key = "n",
-              icon = " ",
-              height = 5,
-              enabled = true,
-            },
-            {
-              title = "Open Issues",
-              cmd = "gh issue list -L 3",
-              key = "i",
-              action = function()
-                vim.fn.jobstart("gh issue list --web", { detach = true })
-              end,
-              icon = " ",
-              height = 7,
-            },
-            {
-              icon = " ",
-              title = "Open PRs",
-              cmd = "gh pr list -L 3",
-              key = "p",
-              action = function()
-                vim.fn.jobstart("gh pr list --web", { detach = true })
-              end,
-              height = 7,
-            },
-            {
-              icon = " ",
-              title = "Git Status",
-              cmd = "git --no-pager diff --stat -B -M -C",
-              height = 10,
-            },
           }
           return vim.tbl_map(function(cmd)
             return vim.tbl_extend("force", {
